b3bc5eb967208930d41b7cffd3bfa492706c5a0a
MarkBCIT <dfs4mark@gmail.com>
2019-02-26 12:01:36 -0800

    123

--- Startup.cs
+++ Startup.cs
@@ -56,7 +56,7 @@ namespace assignment
         options.AddPolicy("RequireAdminRole", policy => policy.RequireRole("Admin"));
     });
 
-
+            services.AddCors();
 
 
 
@@ -97,8 +97,12 @@ namespace assignment
                 // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
                 app.UseHsts();
             }
+       //     app.UseCors(builder =>
+       //builder.WithOrigins("http://localhost:4200"));
             app.UseCors(builder =>
-       builder.WithOrigins("http://localhost:4200"));
+    builder.WithOrigins("http://localhost:4200")
+           .AllowAnyHeader());
+
             app.UseHttpsRedirection();
             app.UseAuthentication();
             app.UseMvc();
