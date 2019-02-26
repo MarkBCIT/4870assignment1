2c3fbcddc17ffd201c152e08d213b6ab81099b46
MarkBCIT <dfs4mark@gmail.com>
2019-02-26 14:14:50 -0800

    123

--- Startup.cs
+++ Startup.cs
@@ -97,8 +97,7 @@ namespace assignment
                 // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
                 app.UseHsts();
             }
-       //     app.UseCors(builder =>
-       //builder.WithOrigins("http://localhost:4200"));
+
             app.UseCors(builder =>
     builder.WithOrigins("http://localhost:4200")
            .AllowAnyHeader());
